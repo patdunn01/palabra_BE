61bcbad549f7021d034156bfffbf8b4093d49ce2
Patrick Dunn <patdunn01@hotmail.co.uk>
2023-05-19 09:31:57 +0100

    adds controllers

--- controllers/controller.js
+++ controllers/controller.js
@@ -2,7 +2,7 @@ const asyncHandler = require('express-async-handler');
 const Word = require("../models/wordBankSchema");
 
 const getWords = asyncHandler(async (req, res) => {
-  const events = await Word.find({});
+  const events = await Word.find();
   res.status(200).json(events);
   });
 
